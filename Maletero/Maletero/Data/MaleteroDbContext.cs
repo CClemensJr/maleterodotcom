52bfbb9a4e3e6bf438473ceed0b0d91e6f0c0795
Deziree' Teague <dez.teague@gmail.com>
2019-02-19 11:37:05 -0800

    created db constructor function

--- Maletero/Maletero/Data/MaleteroDbContext.cs
+++ Maletero/Maletero/Data/MaleteroDbContext.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Maletero.Data
 {
-    public class MaleteroDbContext
+    public class MaleteroDbContext :DbContext 
     {
+        public MaleteroDbContext(DbContextOptions<MaleteroDbContext> options) : base(options)
+        {
+
+        }
     }
 }
