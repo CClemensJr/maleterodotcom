a401fca2cf542f0bfab631ca0e4d0e00e916ebe0
Charles Clemens <cclemensjr@gmail.com>
2019-02-20 10:46:29 -0800

    Added IdentityDbContext

--- Maletero/Maletero/Data/ApplicationDbContext.cs
+++ Maletero/Maletero/Data/ApplicationDbContext.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Maletero.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Maletero.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
     }
 }
