22ef5ea8e028c5f4ba986529280b30f8382e929f
Charles Clemens <cclemensjr@gmail.com>
2019-02-20 13:04:32 -0800

    added properties to view model

--- Maletero/Maletero/Models/ViewModels/RegisterViewModel.cs
+++ Maletero/Maletero/Models/ViewModels/RegisterViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,30 @@ namespace Maletero.Models.ViewModels
 {
     public class RegisterViewModel
     {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
 
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name ="Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The passwords must match")]
+        public string ConfirmPassword { get; set; }
     }
 }
