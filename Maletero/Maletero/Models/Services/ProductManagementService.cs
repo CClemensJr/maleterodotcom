1bc1963a8ab68cfaa30f20f9323f571272ad1319
Deziree' Teague <dez.teague@gmail.com>
2019-02-20 11:13:51 -0800

    added db context to service

--- Maletero/Maletero/Models/Services/ProductManagementService.cs
+++ Maletero/Maletero/Models/Services/ProductManagementService.cs
@@ -1,11 +1,46 @@
-﻿using System;
+﻿using Maletero.Data;
+using Maletero.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Maletero.Models.Services
 {
-    public class ProductManagement
+    public class ProductManagement : IInventory
     {
+        //connect db to service
+
+        private MaleteroDbContext _context { get; }
+
+        Task IInventory.CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IInventory.DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<IEnumerable<Product>> IInventory.GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Product> IInventory.GetbyID(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IInventory.ProductExists(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IInventory.UpdateProduct(Product id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
