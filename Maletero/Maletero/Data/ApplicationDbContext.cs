37d9e138b3182c09b2ae8f7cf6371df2661da74a
Charles Clemens <cclemensjr@gmail.com>
2019-02-20 10:54:30 -0800

    Added summary comments

--- Maletero/Maletero/Data/ApplicationDbContext.cs
+++ Maletero/Maletero/Data/ApplicationDbContext.cs
@@ -10,6 +10,10 @@ namespace Maletero.Data
 {
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        /// <summary>
+        /// This custome constructor is used to bring the DB options into the DbContext
+        /// </summary>
+        /// <param name="options"></param>
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
 
