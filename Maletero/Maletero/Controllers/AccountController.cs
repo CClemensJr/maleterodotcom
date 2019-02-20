88961de4ebdbfe94967f3588dd8d05694cf667cb
Charles Clemens <cclemensjr@gmail.com>
2019-02-20 12:35:51 -0800

    Add Controller class dependency

--- Maletero/Maletero/Controllers/AccountController.cs
+++ Maletero/Maletero/Controllers/AccountController.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Maletero.Controllers
 {
-    public class AccountController
+    public class AccountController : Controller
     {
     }
 }
