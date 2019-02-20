9e32a0faafcd51835d6e34423e628d77e2fb6db3
Deziree' Teague <dez.teague@gmail.com>
2019-02-20 11:34:35 -0800

    brought in interface to product controller

---
+++ Maletero/Maletero/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Maletero.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Maletero.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IInventory _context;
+
+        public ProductController(IInventory context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
