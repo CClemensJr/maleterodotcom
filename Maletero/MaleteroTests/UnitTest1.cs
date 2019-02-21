f2e5fbeab3cc4d6edb1a9c05474051b6f15a38aa
Deziree' Teague <dez.teague@gmail.com>
2019-02-20 22:28:21 -0800

    product SKU tests pass

--- Maletero/MaleteroTests/UnitTest1.cs
+++ Maletero/MaleteroTests/UnitTest1.cs
@@ -23,6 +23,23 @@ namespace MaleteroTests
             Assert.Equal(2, product.ID);
         }
 
+        [Fact]
+        public void CanGetProductSKU()
+        {
+            Product product = new Product();
+            product.SKU = "abc-123";
+            Assert.Equal("abc-123", product.SKU);
+        }
+
+        [Fact]
+        public void CanSetProductSKU()
+        {
+            Product product = new Product();
+            product.SKU = "abc-123";
+            product.SKU = "abc-456";
+            Assert.Equal("abc-456", product.SKU);
+        }
+
         
     }
 }
