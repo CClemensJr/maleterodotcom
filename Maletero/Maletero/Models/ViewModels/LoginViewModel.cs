53448940f9c0a781b65ed5177a2e7c055eff0529
Deziree' Teague <dez.teague@gmail.com>
2019-02-21 10:29:33 -0800

    email property in login view model

--- Maletero/Maletero/Models/ViewModels/LoginViewModel.cs
+++ Maletero/Maletero/Models/ViewModels/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,8 @@ namespace Maletero.Models.ViewModels
 {
     public class LoginViewModel
     {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
     }
 }
