d9095c1bd30785303d34bb14026650c373789f06
Charles Clemens <cclemensjr@gmail.com>
2019-02-21 07:43:49 -0800

    Finished user story 2

--- Maletero/Maletero/Controllers/AccountController.cs
+++ Maletero/Maletero/Controllers/AccountController.cs
@@ -33,7 +33,7 @@ namespace Maletero.Controllers
         public IActionResult Register() => View();
 
         /// <summary>
-        /// Upon form submission this assigns the form values to an ApplicationUser object if the viewmodel is valid. It then redirects them to the home page
+        /// Upon form submission this assigns the form values to an ApplicationUser object if the viewmodel is valid. It then redirects wd to the home page
         /// </summary>
         /// <param name="rvm"></param>
         /// <returns>A view action result</returns>
