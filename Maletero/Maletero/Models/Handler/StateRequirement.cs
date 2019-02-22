49a783b733d7d5341be15d01027d01c8b65071a8
Deziree' Teague <dez.teague@gmail.com>
2019-02-22 11:44:26 -0800

    implemented Iauthorization requirement

--- Maletero/Maletero/Models/Handler/StateRequirement.cs
+++ Maletero/Maletero/Models/Handler/StateRequirement.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Maletero.Models.Handler
 {
-    public class StateRequirement
+    public class StateRequirement : AuthorizationHandler<StateRequirement>, IAuthorizationRequirement
     {
     }
 }
