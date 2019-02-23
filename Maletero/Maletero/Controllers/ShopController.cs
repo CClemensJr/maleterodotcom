0c58c5b0e095b2e161bc65e42ef06a5ea302724f
Charles Clemens <cclemensjr@gmail.com>
2019-02-23 09:48:20 -0800

    Added index action

--- Maletero/Maletero/Controllers/ShopController.cs
+++ Maletero/Maletero/Controllers/ShopController.cs
@@ -11,9 +11,23 @@ namespace Maletero.Controllers
     {
         private readonly IInventory _inventory;
 
+        /// <summary>
+        /// This custom constructor is used to bring in the Inventory interface
+        /// </summary>
+        /// <param name="inventory"></param>
         public ShopController(IInventory inventory)
         {
             _inventory = inventory;
+        }
+
+        /// <summary>
+        /// This method renders the view page when the index route is hit.
+        /// </summary>
+        /// <returns>A View</returns>
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
         }
     }
 }
