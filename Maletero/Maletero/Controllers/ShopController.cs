b36f643124d8c26f5898d493797c96b2813066ae
Charles Clemens <cclemensjr@gmail.com>
2019-02-23 09:43:46 -0800

    Added interface property

--- Maletero/Maletero/Controllers/ShopController.cs
+++ Maletero/Maletero/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Maletero.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +9,6 @@ namespace Maletero.Controllers
 {
     public class ShopController : Controller
     {
-
+        private readonly IInventory _inventory;
     }
 }
