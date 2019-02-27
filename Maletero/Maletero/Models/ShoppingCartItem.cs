68cd38d20d27cdaa1d9d9429bf2502cf1454fc28
Charles Clemens <cclemensjr@gmail.com>
2019-02-27 15:58:45 -0800

    added foreign keys

--- Maletero/Maletero/Models/ShoppingCartItem.cs
+++ Maletero/Maletero/Models/ShoppingCartItem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,5 +11,11 @@ namespace Maletero.Models
     {
         [Key]
         public int ID { get; set; }
+
+        [ForeignKey("ShoppingCart")]
+        public int ShoppingCartID { get; set; }
+
+        [ForeignKey("Product")]
+        public int ProductID { get; set; }
     }
 }
