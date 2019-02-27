4573d4d21dab757e51d017942c8385365b7b207e
Charles Clemens <cclemensjr@gmail.com>
2019-02-27 15:52:13 -0800

    Added primary key

--- Maletero/Maletero/Models/ShoppingCartItem.cs
+++ Maletero/Maletero/Models/ShoppingCartItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,7 @@ namespace Maletero.Models
 {
     public class ShoppingCartItem
     {
+        [Key]
+        public int ID { get; set; }
     }
 }
