2e087a268a3fbf7e15a9e6b3330d36c538992274
Charles Clemens <cclemensjr@gmail.com>
2019-02-27 15:49:41 -0800

    updated properties

--- Maletero/Maletero/Models/ShoppingCart.cs
+++ Maletero/Maletero/Models/ShoppingCart.cs
@@ -9,22 +9,13 @@ namespace Maletero.Models
 {
     public class ShoppingCart
     {
-        //[Key]
-        //public int ID { get; set; }
+        [Key]
+        public int ID { get; set; }
 
-        //[Required]
-        //public int CartID { get; set; }
+        [Required]
+        [EmailAddress]
+        public int UserID { get; set; }
 
-        //[Required]
-        //public int ShopperID { get; set; }
-
-        //[Required]
-        //public Product ProductID { get; set; }
-
-        //[Required]
-        //[Display(Name = "Number of Items")]
-        //public int ProductQuantity { get; set; }
-        
-        //public ICollection<Product> products { get; set; }
+        public ICollection<ShoppingCartItem> ShoppingCartItems { get; set; }
     }
 }
