53922c63cc78889056c1458deeb2dfbf7bf0be45
Charles Clemens <cclemensjr@gmail.com>
2019-02-28 09:52:35 -0800

    Added add property

--- Maletero/Maletero/Models/Interfaces/IShoppingCartManager.cs
+++ Maletero/Maletero/Models/Interfaces/IShoppingCartManager.cs
@@ -7,5 +7,17 @@ namespace Maletero.Models.Interfaces
 {
     public interface IShoppingCartManager
     {
+        // Add a ShoppingCartItem to ShoppingCart
+        Task AddToCart(ShoppingCartItem item);
+
+        // Get a ShoppingCartItem from the ShoppingCart
+
+        // Get all of the ShoppingCartItems from the ShoppingCart
+
+
+        // Update a ShoppingCart
+
+        // Delete a ShoppingCartItem 
+
     }
 }
