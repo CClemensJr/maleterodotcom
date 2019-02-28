8c257b25c2ac073663456ff6a77120fc8d89e96a
Charles Clemens <cclemensjr@gmail.com>
2019-02-28 10:04:23 -0800

    Implemented generic interface

--- Maletero/Maletero/Models/Services/ShoppingCartManagementService.cs
+++ Maletero/Maletero/Models/Services/ShoppingCartManagementService.cs
@@ -1,11 +1,36 @@
-﻿using System;
+﻿using Maletero.Models.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Maletero.Models.Services
 {
-    public class ShoppingCartManagementService
+    public class ShoppingCartManagementService : IShoppingCartManager
     {
+        public Task AddToCart(ShoppingCartItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteCartItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShoppingCartItem> GetACartItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ShoppingCartItem>> GetAllCartItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateCartItems(ShoppingCartItem item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
