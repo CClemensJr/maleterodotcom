8d61d034ac2f81cd949b617fba1f8f42ff567d8b
CClemensJr <cclemensjr@gmail.com>
2019-03-03 13:17:20 -0800

    moved add to cart to shopcontroller

--- Maletero/Maletero/Controllers/ShopController.cs
+++ Maletero/Maletero/Controllers/ShopController.cs
@@ -13,14 +13,18 @@ namespace Maletero.Controllers
     public class ShopController : Controller
     {
         private readonly IInventory _inventory;
+        private readonly IShoppingCartManager _cart;
+        private readonly IShoppingCartItemManager _cartItem;
 
         /// <summary>
         /// This custom constructor is used to bring in the Inventory interface
         /// </summary>
         /// <param name="inventory"></param>
-        public ShopController(IInventory inventory)
+        public ShopController(IInventory inventory, IShoppingCartManager cart, IShoppingCartItemManager cartItem)
         {
             _inventory = inventory;
+            _cart = cart;
+            _cartItem = cartItem;
         }
 
         /// <summary>
@@ -44,6 +48,27 @@ namespace Maletero.Controllers
         public async Task<IActionResult> ShowBag(int id)
         {
             return View(await _inventory.GetbyID(id));
+        }
+
+        [HttpPost]
+        public async Task<RedirectToActionResult> AddToCart(int id)
+        {
+            Product product = await _inventory.GetbyID(id);
+
+            if (product != null)
+            {
+                ShoppingCart shoppingCart = new ShoppingCart();
+
+                shoppingCart.ID = Convert.ToInt32(DateTime.Now);
+                shoppingCart.UserID = "test@test.com";
+
+                shoppingCart.AddProduct(product, 1);
+
+                await _cart.UpdateCart(shoppingCart);
+            }
+
+            return RedirectToAction("/Shop/Index");
+
         }
 
         /// <summary>
