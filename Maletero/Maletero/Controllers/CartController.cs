376a740afd6deaa678b4b7e24839e43f3462a516
CClemensJr <cclemensjr@gmail.com>
2019-03-03 10:54:15 -0800

    Brought in Product interface

--- Maletero/Maletero/Controllers/CartController.cs
+++ Maletero/Maletero/Controllers/CartController.cs
@@ -9,7 +9,22 @@ namespace Maletero.Controllers
 {
     public class CartController : Controller 
     {
+        private readonly IInventory _inventory;
         private readonly IShoppingCartManager _cart;
         private readonly IShoppingCartItemManager _cartItem;
+
+        /// <summary>
+        /// This custom constructor is used to bring in the shoppingcart interfaces
+        /// </summary>
+        /// <param name="cart"></param>
+        /// <param name="cartItem"></param>
+        public CartController(IInventory inventory, IShoppingCartManager cart, IShoppingCartItemManager cartItem)
+        {
+            _inventory = inventory;
+            _cart = cart;
+            _cartItem = cartItem;
+        }
+
+
     }
 }
