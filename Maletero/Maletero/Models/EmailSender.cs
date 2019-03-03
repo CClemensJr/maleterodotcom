9958aee6aaa996823c054902f2595eb5b21a35bf
Deziree' Teague <dez.teague@gmail.com>
2019-03-02 17:52:29 -0800

    set up send email method

--- Maletero/Maletero/Models/EmailSender.cs
+++ Maletero/Maletero/Models/EmailSender.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.Extensions.Configuration;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,6 +17,21 @@ namespace Maletero.Models
         public EmailSender(IConfiguration configuration)
         {
             _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            SendGridClient client = new SendGridClient(_configuration["Sendgrid_Api_Key"]);
+
+            SendGridMessage msg = new SendGridMessage();
+
+            msg.SetFrom("noreply@Maletero.com", "Maletero Admin");
+
+            msg.AddTo(email);
+            msg.SetSubject("Welcome to our Store!");
+            msg.AddContent(MimeType.Html, htmlMessage);
+
+            await client.SendEmailAsync(msg);
         }
     }
 }
