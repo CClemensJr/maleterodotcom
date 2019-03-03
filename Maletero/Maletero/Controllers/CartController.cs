0b79bcf4d7f78cbe89c3c2d5739a3d4695d05f87
CClemensJr <cclemensjr@gmail.com>
2019-03-03 10:40:04 -0800

    created interface properties

--- Maletero/Maletero/Controllers/CartController.cs
+++ Maletero/Maletero/Controllers/CartController.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Maletero.Models.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Maletero.Controllers
 {
-    public class CartController
+    public class CartController : Controller 
     {
+        private readonly IShoppingCartManager _cart;
+        private readonly IShoppingCartItemManager _cartItem;
     }
 }
