965197c2bfbcb927c08ff3f3c38ba92a3ec5773c
Deziree' Teague <dez.teague@gmail.com>
2019-03-03 21:36:54 -0800

    added invoke async method for view component

--- Maletero/Maletero/Models/Components/UserCartItems.cs
+++ Maletero/Maletero/Models/Components/UserCartItems.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿using Maletero.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Maletero.Models.Components
 {
-    public class UserCartItems
+    public class UserCartItems : ViewComponent
     {
+        //may need to change to different db for user cart
+        private MaleteroDbContext _context;
+
+        public UserCartItems(MaleteroDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int number)
+        {
+            var items = _context.Products.OrderByDescending(p => p.ID).Take(number).ToList();
+            return View(items);
+        }
     }
 }
