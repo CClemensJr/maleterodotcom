0ef53223908274be33d34e24e31770861881b248
CClemensJr <cclemensjr@gmail.com>
2019-03-03 17:54:15 -0800

    Merged update and create

--- Maletero/Maletero/Models/Interfaces/IShoppingCartManager.cs
+++ Maletero/Maletero/Models/Interfaces/IShoppingCartManager.cs
@@ -7,18 +7,14 @@ namespace Maletero.Models.Interfaces
 {
     public interface IShoppingCartManager
     {
-        // Create a new cart
-        Task CreateCart(ShoppingCart cart);
-        Task CreateCart();
+        // Create or update a cart
+        Task SaveCart(ShoppingCart cart);
 
         // Get a cart
         Task<ShoppingCart> GetCart(int id);
 
         // Get all carts
         Task<IEnumerable<ShoppingCart>> GetAllCarts();
-
-        // Update a ShoppingCart
-        Task UpdateCart(ShoppingCart cart);
 
         // Delete a ShoppingCart
         Task DeleteCart(int id);
