e2819cab3ec1078e700368f99a14eae20c33d094
Deziree' Teague <dez.teague@gmail.com>
2019-03-04 18:18:23 -0800

    created checkout controller

---
+++ Maletero/Maletero/Controllers/CheckoutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Maletero.Controllers
+{
+    public class CheckoutController: Controller
+    {
+
+    }
+}
