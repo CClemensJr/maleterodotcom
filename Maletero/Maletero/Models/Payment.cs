42fa1881122caa674420b1b11b4823097e80c3bb
Deziree' Teague <dez.teague@gmail.com>
2019-03-05 12:10:15 -0800

    created payment class and installed auth.net package

---
+++ Maletero/Maletero/Models/Payment.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Maletero.Models
+{
+    public class Payment
+    {
+        private IConfiguration _configuration;
+        
+        public Payment(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Run()
+        {
+            return "YAY!";
+        }
+    }
+}
