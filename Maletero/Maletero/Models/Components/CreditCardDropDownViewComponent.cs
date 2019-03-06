54601b96fdc80e600d8e1173e0abcb9a50d72a0d
Charles Clemens <cclemensjr@gmail.com>
2019-03-06 11:04:05 -0800

    Added invoke method

--- Maletero/Maletero/Models/Components/CreditCardDropDownViewComponent.cs
+++ Maletero/Maletero/Models/Components/CreditCardDropDownViewComponent.cs
@@ -8,13 +8,19 @@ namespace Maletero.Models.Components
 {
     public class CreditCardDropDownViewComponent : ViewComponent
     {
-    }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var creditCard = await GetCardAsync();
 
-    public enum CreditCardNumbers:long
-    {
-        AmericanExpress = 370000000000002,
-        Discover = 6011000000000012,
-        Visa = 4007000000027,
-        Mastercard = 5424000000000015
+            return View(creditCard);
+        }
+
+        public enum CreditCardNumbers:long
+        {
+            AmericanExpress = 370000000000002,
+            Discover = 6011000000000012,
+            Visa = 4007000000027,
+            Mastercard = 5424000000000015
+        }
     }
 }
