3e9d6db2bd6b288897ed9f04eea052c93fbabb11
Charles Clemens <cclemensjr@gmail.com>
2019-03-06 10:57:07 -0800

    Added credit card numbers to an enum

--- Maletero/Maletero/Models/Components/CreditCardDropDownViewComponent.cs
+++ Maletero/Maletero/Models/Components/CreditCardDropDownViewComponent.cs
@@ -9,4 +9,12 @@ namespace Maletero.Models.Components
     public class CreditCardDropDownViewComponent : ViewComponent
     {
     }
+
+    public enum CreditCardNumbers:long
+    {
+        AmericanExpress = 370000000000002,
+        Discover = 6011000000000012,
+        Visa = 4007000000027,
+        Mastercard = 5424000000000015
+    }
 }
