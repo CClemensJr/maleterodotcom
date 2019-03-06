2a66650162e8be70117b65435bf9b0e03725a9f7
Deziree' Teague <dez.teague@gmail.com>
2019-03-06 13:25:44 -0800

    created seed data method

--- Maletero/Maletero/Models/RoleInitializer.cs
+++ Maletero/Maletero/Models/RoleInitializer.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Maletero.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +13,17 @@ namespace Maletero.Models
     {
         private static readonly List<IdentityRole> Roles = new List<IdentityRole>()
         {
-            new IdentityRole{Name= ApplicationRoles.}
+            new IdentityRole{Name= ApplicationRoles.Member, NormalizedName = ApplicationRoles.Member.ToUpper(), ConcurrencyStamp = Guid.NewGuid().ToString()},
+            new IdentityRole{Name= ApplicationRoles.Admin, NormalizedName = ApplicationRoles.Admin.ToUpper(), ConcurrencyStamp = Guid.NewGuid().ToString()},
+        };
+
+        public static void SeedData(IServiceProvider serviceProvider)
+        {
+            using (var dbContext = new ApplicationDbContext(serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                dbContext.Database.EnsureCreated();
+                AddRoles(dbContext);
+            }
         }
     }
 }
