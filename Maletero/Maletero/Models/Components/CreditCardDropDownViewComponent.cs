940276e43b518e68f928ac846a8c2c0ab16eb982
Charles Clemens <cclemensjr@gmail.com>
2019-03-06 11:34:37 -0800

    Created a list of card providers

--- Maletero/Maletero/Models/Components/CreditCardDropDownViewComponent.cs
+++ Maletero/Maletero/Models/Components/CreditCardDropDownViewComponent.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -10,6 +11,14 @@ namespace Maletero.Models.Components
     public class CreditCardDropDownViewComponent : ViewComponent
     {
         public string CreditCardProvider { get; set; }
+
+        public List<SelectListItem> CreditCardProviders { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "AMEX", Text = "American Express" },
+            new SelectListItem { Value = "DC", Text = "Discover Card" },
+            new SelectListItem { Value = "MC", Text = "MasterCard"  },
+            new SelectListItem { Value = "VI", Text = "Visa"}
+        };
 
         public async Task<IViewComponentResult> InvokeAsync(string creditCard)
         {
@@ -23,15 +32,15 @@ namespace Maletero.Models.Components
 
         }
 
-        public enum CreditCardProviders
-        {
-            [Display(Name = "American Express")]
-            AMEX,
-            [Display(Name = "Discover Card")]
-            Discover,
-            MasterCard,
-            Visa
-        }
+        //public enum CreditCardProviders
+        //{
+        //    [Display(Name = "American Express")]
+        //    AMEX,
+        //    [Display(Name = "Discover Card")]
+        //    Discover,
+        //    MasterCard,
+        //    Visa
+        //}
 
         public enum CreditCardNumbers:long
         {
