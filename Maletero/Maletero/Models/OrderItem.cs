b9fae3abc93258edaf70499e8c6dcc8d35e77fe0
Deziree' Teague <dez.teague@gmail.com>
2019-03-07 17:47:31 -0800

    created order item class

--- Maletero/Maletero/Models/OrderItem.cs
+++ Maletero/Maletero/Models/OrderItem.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,39 @@ namespace Maletero.Models
 {
     public class OrderItem
     {
+        [Key]
+        public int ID { get; set; }
+
+        [ForeignKey("Order")]
+        public int OrderID { get; set; }
+
+        [ForeignKey("Product")]
+        [Required]
+        public Product Product { get; set; }
+
+        [Required]
+        public int ProductID { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public OrderItem() { }
+
+        /// <summary>
+        /// This is a custom constructor that allows properties to be set upon instantiation
+        /// </summary>
+        /// <param name="cartID"></param>
+        /// <param name="product"></param>
+        /// <param name="quantity"></param>
+        public OrderItem(int orderID, Product product, int quantity)
+        {
+            OrderID = orderID;
+            Product = product;
+            Quantity = quantity;
+            ProductID = Product.ID;
+        }
     }
 }
