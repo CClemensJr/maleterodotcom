c0cc019c3740d2a8f6c983891deaa2eca1c11731
Deziree' Teague <dez.teague@gmail.com>
2019-03-07 18:19:26 -0800

    updated order and order item classes

--- Maletero/Maletero/Models/Interfaces/IOrderItemsManager.cs
+++ Maletero/Maletero/Models/Interfaces/IOrderItemsManager.cs
@@ -7,5 +7,10 @@ namespace Maletero.Models.Interfaces
 {
     public class IOrderItemsManager
     {
+        // Get a order item
+        Task<ShoppingCart> GetOrderItem(string userName);
+
+        // Get all order items
+        Task<IEnumerable<ShoppingCart>> GetAllOrderItems();
     }
 }
--- Maletero/Maletero/Models/Interfaces/IOrderManager.cs
+++ Maletero/Maletero/Models/Interfaces/IOrderManager.cs
@@ -7,5 +7,12 @@ namespace Maletero.Models.Interfaces
 {
     public class IOrderManager
     {
+      
+        // Get an order
+        Task<ShoppingCart> GetOrder(string userName);
+
+        // Get all orders
+        Task<IEnumerable<Order>> GetAllOrders();
+
     }
 }
