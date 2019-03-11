21c1842cce5eb29288fdadca276d9cd0d3035a1a
CClemensJr <cclemensjr@gmail.com>
2019-03-10 21:10:57 -0700

    Added configuration dependency

--- Maletero/Maletero/Controllers/CartController.cs
+++ Maletero/Maletero/Controllers/CartController.cs
@@ -2,6 +2,7 @@
 using Maletero.Models.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,17 +15,19 @@ namespace Maletero.Controllers
         private readonly IInventory _inventory;
         private readonly IShoppingCartManager _cart;
         private readonly IShoppingCartItemManager _cartItem;
+        private IConfiguration _configuration;
 
         /// <summary>
         /// This custom constructor is used to bring in the shoppingcart interfaces
         /// </summary>
         /// <param name="cart"></param>
         /// <param name="cartItem"></param>
-        public CartController(IInventory inventory, IShoppingCartManager cart, IShoppingCartItemManager cartItem)
+        public CartController(IInventory inventory, IShoppingCartManager cart, IShoppingCartItemManager cartItem, IConfiguration configuration)
         {
             _inventory = inventory;
             _cart = cart;
             _cartItem = cartItem;
+            _configuration = configuration;
         }
 
         /// <summary>
@@ -36,6 +39,22 @@ namespace Maletero.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _cartItem.GetAllCartItems());
+        }
+
+        [HttpPost]
+        public IActionResult Index(bool works = true)
+        {
+            Payment payment = new Payment(_configuration);
+            string answer = payment.Run();
+
+            if (answer == "Ok")
+            {
+                return View();
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
